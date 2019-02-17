6d46d38c6e4fef2788f70889af074f1afbc52465
Lennard Kwakernaak <lennardkwakernaak@gmail.com>
2019-02-17 16:30:04 +0100

    COLORS and an FPS-counter

--- viewport.py
+++ viewport.py
@@ -11,52 +11,76 @@ class Viewport(pyglet.window.Window):
         super().__init__()
         self.gamestate = gamestate
         self.fpsdisplay = pyglet.window.FPSDisplay(self)
-        # self.batch = pyglet.graphics.Batch()
 
         self.rotation = [1, 0]
+        self.colors = None
 
     def on_draw(self):
         self.clear()
+        self.batch = pyglet.graphics.Batch()
         self.set_3d()
+        self.particles_to_batch()
+        # self.bounding_box_to_batch()
+        self.batch.draw()
 
+        self.set_2d()
+
+        self.draw_fps()
+
+    def draw_bounding_box(self):
+        verts, faces = cube()
+        verts /=2
+        # verts /= self.gamestate.size[0]
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)
+        self.batch.add_indexed(
+            len(verts),
+            gl.GL_TRIANGLES,
+            None,
+            faces.flatten(),
+            ('v3f', verts.flatten())
+        )
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL)
+
+    def particles_to_batch(self):
         gamestate = self.gamestate
-        print(gamestate.positions.shape, gamestate.size)
-        positions = 10*(gamestate.positions-np.asarray(gamestate.size)/3) / np.asarray(gamestate.size)[np.newaxis, :]
+        # print(gamestate.positions.shape, gamestate.size)
 
-        batch = pyglet.graphics.Batch()
-
+        # positions = gamestate.positions - np.asarray(gamestate.size)/2
+        positions = (gamestate.positions - np.asarray(gamestate.size) / 2) / np.asarray(gamestate.size)[np.newaxis,:]
         verts, faces = icosahedron()  # The vertices and faces of a single icosahedron
-        verts/=20                     # Scaling down size of ico. Should probably be scaled down to sigma
+        print(verts)
+        verts = verts/ 100  # Scaling down size of ico. Should probably be scaled down to ~sigma
         verts_per_shape = len(verts)
 
         particles = gamestate.particles
         dimensions = gamestate.dimensions
 
-
-        verts = np.tile(verts.T,particles).T  # create vertices for all particles
-
-        verts[:,:min(3,dimensions)] += positions.repeat(verts_per_shape).reshape(dimensions,-1).T
+        verts = np.tile(verts.T, particles).T  # create vertices for all particles
+        verts[:, :min(3, dimensions)] += positions.repeat(verts_per_shape).reshape(dimensions, -1).T
 
         faces = (faces.flatten()[:, np.newaxis] + verts_per_shape * np.arange(particles)).T.flatten()
-
         faces = faces.flatten()
-        spheres = batch.add_indexed(len(verts), gl.GL_TRIANGLES, None, faces,
-                                    ('v3f', verts.flatten()))
 
-        batch.draw()
+        if self.colors is None:
+            self.colors = (verts.T/np.sum(verts, axis=1)).T
+        spheres = self.batch.add_indexed(
+            len(verts),
+            gl.GL_TRIANGLES,
+            None,
+            faces,
+            ('v3f', verts.flatten()),
+            ('c3f', self.colors.flatten())
+        )
 
-        gl.glMatrixMode(gl.GL_PROJECTION)
-        gl.glLoadIdentity()
-        gl.glOrtho(-5, 5, -5, 5, -5, 5)
+    def draw_fps(self):
+        label = pyglet.text.Label('States per second: {:.2f}'.format(pyglet.clock.get_fps() * self.gamestate.drawevery),
+                                  font_name='Times New Roman',
+                                  font_size=12,
+                                  x=10, y=15,
+                                  anchor_x='left', anchor_y='top',
+                                  color=(0, 0, 0, 255))
+        label.draw()
 
-        # self.set_2d()
-        #
-        # label = pyglet.text.Label('States per second: {:.2f}'.format(pyglet.clock.get_fps() * self.gamestate.drawevery),
-        #                           font_name='Times New Roman',
-        #                           font_size=12,
-        #                           x=10, y=10,
-        #                           anchor_x='left', anchor_y='bottom')
-        # label.draw()
 
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
         # if buttons & self.mouse.LEFT:
@@ -70,6 +94,8 @@ class Viewport(pyglet.window.Window):
         width, height = self.get_size()
         gl.glDisable(gl.GL_DEPTH_TEST)
         gl.glViewport(0, 0, width, height)
+
+        # Ortho projection
         gl.glMatrixMode(gl.GL_PROJECTION)
         gl.glLoadIdentity()
         gl.glOrtho(0, width, 0, height, -1, 1)
@@ -82,9 +108,15 @@ class Viewport(pyglet.window.Window):
         width, height = self.get_size()
         gl.glEnable(gl.GL_DEPTH_TEST)
         gl.glViewport(0, 0, width, height)
-        gl.glMatrixMode(gl.GL_MODELVIEW)
+
+        # set perspective transformation to unity
+        gl.glMatrixMode(gl.GL_PROJECTION)
         gl.glLoadIdentity()
-        glu.gluPerspective(65.0, width / float(height), 0.1, 60.0)
+        glu.gluPerspective(70.0, width / float(height), 1, 50)
+
+        gl.glTranslatef(0,0,-2)
+
+        # set modelview rotation
         gl.glMatrixMode(gl.GL_MODELVIEW)
         gl.glLoadIdentity()
 
@@ -92,7 +124,8 @@ class Viewport(pyglet.window.Window):
         gl.glRotatef(theta, 0, 1, 0)
         gl.glRotatef(-phi, np.cos(np.deg2rad(theta)), 0, np.sin(np.deg2rad(phi)))
 
-        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)
+
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL)
 
 
 def setup():
@@ -113,6 +146,41 @@ def triangle():
     faces = np.asarray([
         [0, 1, 2]
     ])
+    return vertices, faces
+
+def cube():
+    vertices = np.array([
+        [-1.,-1,-1],
+        [1,-1,-1],
+        [1,1,-1],
+        [-1,1,-1],
+
+        [-1,-1,1],
+        [1,-1,1],
+        [1,1,1],
+        [-1,1,1]
+    ])
+
+    faces = np.array([
+        [0,1,2],
+        [2,3,0],
+
+        [3,2,6],
+        [6,7,3],
+
+        [7,6,5],
+        [5,4,7],
+
+        [3,7,4],
+        [4,0,3],
+
+        [6,2,1],
+        [1,5,6],
+
+        [4,5,1],
+        [1,0,4]
+    ])
+
     return vertices, faces
 
 def icosahedron():
@@ -142,7 +210,7 @@ def icosahedron():
         [0, 11, 5],
         [0, 5, 1],
         [0, 1, 7],
-        [0, 7, 0],
+        [0, 7, 10],
         [0, 10, 11],
 
         [1, 5, 9],
