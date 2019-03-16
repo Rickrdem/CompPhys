922e6627e594744fd60a5df072c19151f78a7a88
Rickrdem <36420046+Rickrdem@users.noreply.github.com>
2019-03-16 10:54:32 +0100

    teperature-time implementation

--- gamestate.py
+++ gamestate.py
@@ -8,41 +8,39 @@ class Gamestate():
         :return:
         """
         self.positions = state.copy()
+        self.original_positions = state.copy()
         self.positions_not_bounded = state.copy()
         self.velocities[:] = np.random.normal(0, 0.1,size=(self.particles,self.dimensions))
         self.velocities[:] = self.velocities - np.average(self.velocities, axis=0)[None,:]
 
     def __init__(self, state, h=0.005, T=0.5, size=(10,10,10), dtype=np.float32):
         self.h = h
-        self.m = 1        
         self.T = T
         self.time = 0
-        self.dt = 0
 
         self.size = size
         self.particles = np.shape(state)[0]
         self.dimensions = np.shape(state)[1]
 
         self.volume = self.size[0]*self.size[1]*self.size[2]
-        self.pressure = self.particles * self.T * 119.8 / self.volume
+        self.pressure = self.particles * self.T / self.volume
         self.density = self.particles/self.volume
         self.measured_temperature = self.T
         self.thermostat = True
         
         self.dtype = dtype
-        self.original_positions = state.copy()
-
+        
         self.positions = np.zeros([self.particles,self.dimensions], dtype=dtype)
+        self.original_positions = np.zeros([self.particles,self.dimensions], dtype=dtype)
         self.velocities = np.zeros([self.particles,self.dimensions], dtype=dtype)
         self.forces = np.zeros([self.particles,self.dimensions], dtype=dtype)
-        self.previous_forces = np.zeros([self.particles,self.dimensions], dtype=dtype)
         self.distances = np.zeros([self.particles, self.particles, self.dimensions], dtype=dtype)
         
         self.potential_energy = []
         self.kinetic_energy = []
         self.diffusion = []       
-        self.pair_correlation = []
-        
+        self.temperature = []
+#        self.dt = 0       
 #        self.maxdist = 0
         
         
@@ -56,30 +54,26 @@ class Gamestate():
         """
 
         self.velocities_update()  # first half
+
         if self.thermostat:
             Lambda = np.sqrt(((self.particles-1)*3*self.T)/(np.sum(np.square(self.velocities))))
+            self.velocities = Lambda * self.velocities
             if abs(1-Lambda) < 0.0001:
                 self.thermostat = False
-        else:
-            Lambda = 1
-        print(Lambda)    
-        self.velocities = Lambda * self.velocities
         
         self.positions_update()
         self.distances_update()
         self.forces_update()
         self.velocities_update()  # seconds half
         
-
-        self.positions[:, :] %= np.asarray(self.size)[np.newaxis, :]
-        self.kinetic_energy.append(np.sum(1/2*self.m*np.square(self.velocities))) 
-        
         self.measured_temperature = np.average((np.square(self.velocities)))
 
+        self.positions[:, :] %= np.asarray(self.size)[np.newaxis, :]
+        
+        self.kinetic_energy.append(np.sum(1/2*np.square(self.velocities))) 
         self.potential_energy.append(func.sum_potential_jit(self.distances))
-
         self.diffusion.append(np.average(np.square((self.positions_not_bounded - self.original_positions))))
-
+        self.temperature.append([self.T, self.measured_temperature])
 
         """Find maximum possible distance between two atoms"""
 #        y = np.max(np.sqrt(np.sum(np.square(self.distances), axis=2)))
@@ -92,7 +86,7 @@ class Gamestate():
 #            self.dt = 0
 #            self.T += 0.05
         
-        self.time += 2.15*self.h
+        self.time += self.h
         
         
     def positions_update(self):
