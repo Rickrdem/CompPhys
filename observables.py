9022c356644ca98520f992e9c322dd67c565267a
Rickrdem <36420046+Rickrdem@users.noreply.github.com>
2019-03-24 17:32:04 +0100

    axes labeled and commented

--- observables.py
+++ observables.py
@@ -7,13 +7,15 @@ import functions as func
 
 
 def energy(game):
+    """Timetrace of the energy in the system. Kinetic energy, potential enery 
+    and the sum of those is displayed"""
     fig, ax = plt.subplots()
-    time = np.arange(game.h, 2.15*game.h*len(game.kinetic_energy), 2.15*game.h)
+    time = np.arange(0, game.h*len(game.kinetic_energy), game.h)
     ax.plot(time, np.array(game.kinetic_energy[:]),c='r', label='$E_{kin}$')
     ax.plot(time, game.potential_energy, c='b', label='$E_{pot}$')
-    ax.plot(time, np.asarray(game.kinetic_energy)+game.potential_energy, c='black', label='H')
-    ax.set_xlabel("Time (ns)")
-    ax.set_ylabel("Energy ($1/\epsilon$)")
+    ax.plot(time, np.asarray(game.kinetic_energy)+game.potential_energy, c='black', label='$E_{tot}$')
+    ax.set_xlabel(r"$t/\tau$")
+    ax.set_ylabel(r"$E/\epsilon$")
     ax.legend()
     fig.tight_layout()
     fig.show()
@@ -24,40 +26,42 @@ def velocity_distribution(game):
 
     velocities = func.abs(game.velocities)
         
-    y, bin_edges = np.histogram(velocities, bins=int(np.sqrt(len(velocities))))
+    y, bin_edges = np.histogram(velocities, bins=int(np.sqrt(len(velocities))), density=True)
     bincenters = 0.5*(bin_edges[1:] + bin_edges[:-1])
-    menStd     = np.sqrt(y)
+    menStd     = np.sqrt(y)/np.sum(y)
     width      = np.max(velocities)/int(np.sqrt(len(velocities)))
     ax.bar(bincenters, y, width=width, color='b', yerr=menStd)
     
-#    maxwell = stats.maxwell
-#    params = maxwell.fit(velocities, floc=0)
-#    x = np.linspace(0, np.max(velocities), 100)
-#    ax.plot(x, maxwell.pdf(x, *params), color='black', lw=3)   
+    maxwell = stats.maxwell
+    params = maxwell.fit(velocities, floc=0)
+    x = np.linspace(0, np.max(velocities), 100)
+    ax.plot(x, maxwell.pdf(x, *params), color='black', lw=3)   
     
     
     ax.set_xlabel("Velocity")
     ax.set_ylabel("#/N")
-#    ax.set_title("(params)="+str(params))
+    ax.set_title("(params)="+str(params))
     fig.tight_layout()
     fig.show()
 
 def diffusion(game):
+    """Timetrace of the diffusion in the system."""
     fig, ax = plt.subplots()
-    time = np.arange(game.h, 2.15*game.h*len(game.diffusion), 2.15*game.h)
+    time = np.arange(0, game.h*len(game.diffusion), game.h)
     
     p, cov = np.polyfit(x=time, y=game.diffusion, deg=1, full=False, cov=True)
    
     ax.plot(time, game.diffusion, c='b', label='$Diffusion$')
     ax.plot(time, p[0]*time+p[1], color='black', label='linear fit')
-    ax.set_xlabel("Time (ns)")
-    ax.set_ylabel("$<(x(t)-x(0))^2>$")
-    ax.set_title("Fit: $<(x(t)-x(0))^2>$=2Dt+b; D={D:.4f}$\pm$ {std:.4f}, b={b:.3f}".format(D=p[0]/2, std=np.sqrt(cov[0,0]), b=p[1]))
+    ax.set_xlabel(r"$t/\tau$")
+    ax.set_ylabel(r"$<(r(t)-r(0))^2>$")
+    ax.set_title("Fit: $<(r(t)-r(0))^2>$=2Dt+b; D={D:.4f}$\pm$ {std:.4f}, b={b:.3f}".format(D=p[0]/2, std=np.sqrt(cov[0,0]), b=p[1]))
     ax.legend()
     fig.tight_layout()
     fig.show()
 
 def pair_correlation(game, fig=None, ax=None):
+    """Plot of the pair correlation function"""
     dr = 1/game.particles**(1/2)
     radius = np.arange(0.4, game.size[0], dr)
     pair_correlation = []
@@ -69,17 +73,19 @@ def pair_correlation(game, fig=None, ax=None):
         n_r = np.sum(Z, axis=1)
         g_r = 2*game.volume/(game.particles*(game.particles-1))*np.average(n_r)/(4*np.pi*r*r*dr)
         pair_correlation.append(g_r)
-      
-#    fig, ax = plt.subplots()
+    if fig is None or ax is None:
+        fig, ax = plt.subplots()
     ax.plot(radius, pair_correlation/(np.sum(pair_correlation)*dr), c='b', label='Pair Correlation')
-    ax.set_xlabel("r/$\sigma$")
-    ax.set_ylabel("g(r/$\sigma$)")
+    ax.set_xlabel(r"r/$\sigma$")
+    ax.set_ylabel(r"g(r/$\sigma$)")
     ax.legend()
     fig.tight_layout()
     fig.show()
   
 def temperature(game):
-    time = np.arange(game.h, 2.15*game.h*len(game.kinetic_energy), 2.15*game.h)
+    """Timetrace of the temperature of the system."""
+
+    time = np.arange(0, game.h*len(game.kinetic_energy), game.h)
     temp = np.array(game.temperature)
     set_temp = np.array(game.set_temperature)
     
@@ -104,8 +110,8 @@ def temperature(game):
     
     ax2.plot(y, bins, 'r--', linewidth=2, label='pdf')
 
-    ax.set_xlabel("Time (ns)")
-    ax.set_ylabel("Temparature")
+    ax.set_xlabel(r"$t/\tau$")
+    ax.set_ylabel(r"$T$")
     ax2.set_xlabel("Probability density")
     
     ax2.set_title("$\mu$, $\sigma$ = {m:.2f}, {s:.2f}".format(m=mu, s=sigma))
