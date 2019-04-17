c04bb723a13d8b67007b1774560c77e979ba75ca
Rickrdem <36420046+Rickrdem@users.noreply.github.com>
2019-04-17 17:41:18 +0200

    reset button

--- Monte Carlo/viewer.py
+++ Monte Carlo/viewer.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.widgets import *
 from matplotlib import cm
+import matplotlib.patches as mpatches
 import time
 import matplotlib.animation as animation
 
@@ -11,14 +12,20 @@ class Viewer():
     TEMP_STEP = 1e-4
     MINCOUPLING = -5
     MAXCOUPLING = 5
-    COUPLING_STEP = 0.1
+    COUPLING_STEP = 0.01
     MINFIELD = -5
     MAXFIELD = 5
-    FIELD_STEP = 0.1    
+    FIELD_STEP = 0.01    
     def __init__(self, simulation_state, update_every=1):
         # self.fig, self.ax = plt.subplots()
         self.simulation_state = simulation_state
         self.update_every = update_every
+        
+        
+        self.initial_temperature = self.simulation_state.T
+        self.initial_paircoupling = self.simulation_state.J
+        self.initial_field = self.simulation_state.magnetic_field
+
 
         self._init_matplotlib()
 
@@ -28,6 +35,12 @@ class Viewer():
 
         plt.subplots_adjust(left=0.25, bottom=0.25)
 
+        values = self.simulation_state.spinchoice
+        colors = [ self.ax.cmap(self.ax.norm(value)) for value in values]
+        patches = [ mpatches.Patch(color=colors[i], label="Spin {l}".format(l=values[i]) ) for i in range(len(values)) ]
+        plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., fancybox=True, shadow=True )
+        
+        #Siders
         self.axtemp = self.fig.add_axes([0.25, 0.15, 0.65, 0.03], facecolor='lightgoldenrodyellow')
         self.temp_slider = Slider(self.axtemp, 'Temperature', self.MINTEMP, self.MAXTEMP, 
                                   valinit=self.simulation_state.T, valstep=self.TEMP_STEP)
@@ -40,11 +53,20 @@ class Viewer():
         self.field_slider = Slider(self.axfield, 'Magnetic Field', self.MINFIELD, self.MAXFIELD, 
                                       valinit=self.simulation_state.magnetic_field, valstep=self.FIELD_STEP)
 
+        
+        self.resetax = self.fig.add_axes([0.85, 0.20, 0.1, 0.04])
+        self.reset_button = Button(self.resetax, 'Reset', color='lightgoldenrodyellow', hovercolor='0.975')
+
+
+
         self.animation_handler = animation.FuncAnimation(self.fig, self.update, blit=True, interval=5)
 
     def update(self, *args):
         self.simulation_state.T = float(self.temp_slider.val)
         self.simulation_state.J = float(self.coupling_slider.val)
+        self.simulation_state.magnetic_field = float(self.field_slider.val)
+        self.reset_button.on_clicked(self.reset)
+        
         for i in range(self.update_every):
             self.simulation_state.update()
         self._update_fig()
@@ -53,6 +75,13 @@ class Viewer():
     def _update_fig(self):
         self.ax.set_array(self.simulation_state.state)
 
+    def reset(self, event):
+        self.simulation_state.T = self.initial_temperature
+        self.simulation_state.J = self.initial_paircoupling
+        self.simulation_state.magnetic_field = self.initial_field
+        self.temp_slider.reset()
+        self.coupling_slider.reset()
+        self.field_slider.reset()
 
 if __name__ == '__main__':
     viewer = Viewer(None)
