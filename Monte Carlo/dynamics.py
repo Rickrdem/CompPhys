47ef50ca55494e08b99ba5c7265d7b087efdb2a4
Rickrdem <36420046+Rickrdem@users.noreply.github.com>
2019-04-17 16:54:36 +0200

    Slider for magnetization and pair-pair coupling

--- Monte Carlo/dynamics.py
+++ Monte Carlo/dynamics.py
@@ -11,6 +11,7 @@ class Dynamics():
         self.spinchoice = [1,-1]
         self.J = J
         self.T = T
+        self.magnetic_field = 1
 
         self.neighbours = np.array([[((i-1)%self.rows, j), ((i+1)%self.rows, j),
                                    (i, (j-1)%self.columns), (i,(j+1)%self.columns)]
@@ -30,7 +31,7 @@ class Dynamics():
         self.state = np.random.choice(self.spinchoice, (self.rows,self.columns))
     
     def update(self):
-        self.state, energy_chunk = func.metropolis(self.state, self.neighbours, self.T, steps=self.columns*20)
+        self.state, energy_chunk = func.metropolis(self.state, self.neighbours, self.T, steps=1)#self.columns*20)
         self.energy.extend(energy_chunk)
         
         self.m = np.average(self.state)
--- Monte Carlo/viewer.py
+++ Monte Carlo/viewer.py
@@ -9,6 +9,12 @@ class Viewer():
     MINTEMP = 1E-10
     MAXTEMP = 4
     TEMP_STEP = 1e-4
+    MINCOUPLING = -5
+    MAXCOUPLING = 5
+    COUPLING_STEP = 0.1
+    MINFIELD = -5
+    MAXFIELD = 5
+    FIELD_STEP = 0.1    
     def __init__(self, simulation_state, update_every=1):
         # self.fig, self.ax = plt.subplots()
         self.simulation_state = simulation_state
@@ -16,23 +22,33 @@ class Viewer():
 
         self._init_matplotlib()
 
+    def _init_matplotlib(self):
+        self.fig = plt.figure()
+        self.ax = plt.imshow(self.simulation_state.state, cmap=cm.binary, animated=True)
+
+        plt.subplots_adjust(left=0.25, bottom=0.25)
+
+        self.axtemp = self.fig.add_axes([0.25, 0.15, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+        self.temp_slider = Slider(self.axtemp, 'Temperature', self.MINTEMP, self.MAXTEMP, 
+                                  valinit=self.simulation_state.T, valstep=self.TEMP_STEP)
+
+        self.axcoupling = self.fig.add_axes([0.25, 0.10, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+        self.coupling_slider = Slider(self.axcoupling, 'Pair-Coupling', self.MINCOUPLING, self.MAXCOUPLING, 
+                                      valinit=self.simulation_state.J, valstep=self.COUPLING_STEP)
+        
+        self.axfield = self.fig.add_axes([0.25, 0.05, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+        self.field_slider = Slider(self.axfield, 'Magnetic Field', self.MINFIELD, self.MAXFIELD, 
+                                      valinit=self.simulation_state.magnetic_field, valstep=self.FIELD_STEP)
+
+        self.animation_handler = animation.FuncAnimation(self.fig, self.update, blit=True, interval=5)
+
     def update(self, *args):
         self.simulation_state.T = float(self.temp_slider.val)
+        self.simulation_state.J = float(self.coupling_slider.val)
         for i in range(self.update_every):
             self.simulation_state.update()
         self._update_fig()
         return self.ax,
-
-    def _init_matplotlib(self):
-        self.fig = plt.figure()
-        self.ax = plt.imshow(self.simulation_state.state, animated=True)
-        # animation.FuncAnimation(self.fig, self.simulation_state.update, 1, interval=1, blit=True)
-        plt.subplots_adjust(left=0.25, bottom=0.25)
-
-        self.axtemp = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor='lightgoldenrodyellow')
-        self.temp_slider = Slider(self.axtemp, 'Temperature', self.MINTEMP, self.MAXTEMP, valinit=1, valstep=self.TEMP_STEP)
-        # self.ax.imshow(self.simulation_state.state, cmap=cm.binary, animated=True)
-        self.animation_handler = animation.FuncAnimation(self.fig, self.update, blit=True, interval=5)
 
     def _update_fig(self):
         self.ax.set_array(self.simulation_state.state)
