4b8773e22f3e7fa5d71a04c9ef800057f7a2904a
Lennard Kwakernaak <lennardkwakernaak@gmail.com>
2019-04-25 14:00:32 +0200

    Added tim_data bootstrap and some comments

--- Monte Carlo/observables.py
+++ Monte Carlo/observables.py
@@ -6,38 +6,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.ndimage as ndi
 
+def trim_data(simulation_state, start, end=None):
+    """
+    Trim the data to remove a set of time steps.
+    :param simulation_state: The state to trim in time
+    :param start: The number of states to trim off of the front of the dataset.
+    :param end: The number of states to trim off of the back of the dataset.
+    :return: Trimmed simulation state.
+    """
+    simulation_state.energy = simulation_state.energy[start:end]
+    simulation_state.magnetization = simulation_state.magnetization[start:end]
+    return simulation_state
+
 def magnetization(simulation_state):
     """
     Plots the average magnetization of the system. 
     """
     magnetization = simulation_state.magnetization
+    average_magnetization = np.average(magnetization)
+    magnetization_error = bootstrap(magnetization, np.average)
     time = np.arange(0, len(magnetization), 1)
     
     fig, ax = plt.subplots()
     ax.plot(time, magnetization)
     
     ax.set_xlabel("Time")
-    ax.set_ylabel(r"$\langle m \rangle$")
+    ax.set_ylabel("m")
     
     fig.tight_layout()
     fig.show()
+    return average_magnetization, magnetization_error
     
 def susceptibility(simulation_state):
     """
-    Plots the susceptibility per spin. 
+    Calculates the susceptibility per spin.
     """
-    susceptibility = np.var(simulation_state.magnetization)*simulation_state.N/simulation_state.T
-    print(susceptibility)
-    # time = np.arange(0, len(susceptibility), 1)
-    #
-    # fig, ax = plt.subplots()
-    # ax.plot(time, susceptibility)
-    #
-    # ax.set_xlabel("Time")
-    # ax.set_ylabel(r"$\chi$ ($k_BT$)")
-    #
-    # fig.tight_layout()
-    # fig.show()
+    func = lambda x: np.var(x)*simulation_state.N/simulation_state.T
+    sus = func(simulation_state.magnetization)
+    sus_error = bootstrap(simulation_state.magnetization, func)
+    return sus, sus_error
+
+def specific_heat(simulation_state):
+    """
+    Calculates the specific heat of the system.
+    """
+    func = lambda x: np.var(x)/simulation_state.N/(simulation_state.T**2)
+    s_heat = func(simulation_state.energy)
+    s_heat_err = bootstrap(simulation_state.energy, func)
+
+    return s_heat, s_heat_err
 
 def nn_interaction(shape):
     middle = len(shape)//2
@@ -50,3 +67,12 @@ def energy(state, J=1, H=0):
     footprint = np.array([[0,1,0],[1,1,1],[0,1,0]])
     return -J * np.sum(ndi.generic_filter(
         state, function=nn_interaction, footprint=footprint, mode='wrap'))
+
+def bootstrap(data, function, n=100):
+    """Bootstrap an observable, given a specific function"""
+    observables = []
+    for i in range(n):
+        resampled = np.random.choice(data, size=len(data), replace=True)
+        observables.append(function(resampled))
+    return np.std(observables)
+
