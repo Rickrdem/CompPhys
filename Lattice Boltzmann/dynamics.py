a1c65e6b1f28281d3b70a5b4b739b94cfc738fcd
Lennard Kwakernaak <lennardkwakernaak@gmail.com>
2019-05-03 12:39:54 +0200

    Added the basic collision calculation step

--- Lattice Boltzmann/dynamics.py
+++ Lattice Boltzmann/dynamics.py
@@ -1,6 +1,24 @@
+import numpy as np
 
 class Dynamics():
-    def __init__(self):
-        pass
+    def __init__(self, xsize=520, ysize=180):
+        self.n_neighbours = 9  # simple square lattice
+        self.shape = (xsize,ysize, n_neighbours)
+        self.flowin = np.zeros(shape)
+        self.flowout = flowin.copy()
+        self.floweq = flowin.copy()
+
+        self.reynolds = 220.
+        self.influx_v = 0.4  # the velocity in lattice units of the ingoing flow
+
+        # self.lattice_velocity = [(x,y) for y in [-1,0,1] for x in [-1,0,1]]
+        #
+        # self.lattice_weight = [1/9 for i in range(9)]
+        # self.lattice_weight[::2] = [1/36 for i in self.lattice_weight[::2]]
+        # self.lattice_weight[4] = 4/9
+
+        self.masked_off = np.zeros((self.shape[0], self.shape[1]), dtype=bool)
+        self.masked_off[[30:60,30:60]] = True
+
     def update(self):
         pass
--- Lattice Boltzmann/latticeboltzmann.py
+++ Lattice Boltzmann/latticeboltzmann.py
@@ -1,6 +1,38 @@
+import numpy as np
+from numba import njit
 
-def collision(content):
-    pass
+lattice_velocity = np.array([(x, y) for y in [-1, 0, 1] for x in [-1, 0, 1]], dtype=np.float)
+
+lattice_weight = np.array([1 / 9 for i in range(9)])
+lattice_weight[::2] = [1 / 36 for i in lattice_weight[::2]]
+lattice_weight[4] = 4 / 9
+
+VELOCITY_C = 1 / np.sqrt(3)
+TIME_C = 1
+
+@njit()
+def collision(flow):
+    flow = flow.copy()
+    density = 0
+    velocity = np.array([0.,0.])
+    for i in range(9):
+        density += flow[i]
+        velocity += flow[i]*lattice_velocity[i]
+    velocity/=density
+
+    flow_eq = []
+    for i in range(9):
+        product = lattice_velocity[i] @ velocity.T
+        velocity_squared = velocity @ velocity.T
+        flow_eq.append(
+            lattice_weight[i]*density*(
+                1 + product / VELOCITY_C ** 2 + \
+                np.square(product) / (2 * VELOCITY_C ** 4) -\
+                velocity_squared/(2 * VELOCITY_C**2)
+            )
+        )
+        flow[i] -= 1/TIME_C * (flow[i] - flow_eq[i])
+    return flow
 
 def streaming(content, neighbours):
     pass
