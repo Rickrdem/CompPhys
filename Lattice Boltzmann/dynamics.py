7e56d6d2786261cc809a625d4fd0bb60eb77cbb5
Lennard Kwakernaak <lennardkwakernaak@gmail.com>
2019-05-08 12:03:57 +0200

    Added the streaming, collision and mask step.
    The boundary stuff isn't working yet. At the left wall there should
    constantly be a flow of the same state. On the right wall there should
    be removed flow and the periodic boundary indexing has te be removed.

--- Lattice Boltzmann/dynamics.py
+++ Lattice Boltzmann/dynamics.py
@@ -1,24 +1,28 @@
 import numpy as np
+from latticeboltzmann import collision, streaming, flow_equilibrium, update
 
 class Dynamics():
     def __init__(self, xsize=520, ysize=180):
         self.n_neighbours = 9  # simple square lattice
-        self.shape = (xsize,ysize, n_neighbours)
-        self.flowin = np.zeros(shape)
-        self.flowout = flowin.copy()
-        self.floweq = flowin.copy()
+        self.shape = (xsize, ysize, self.n_neighbours)
+        self.flowin = np.zeros(self.shape)
+        self.flowout = self.flowin.copy()
+        self.floweq = self.flowin.copy()
+        self.velocity = np.empty(xsize, ysize, 2)
 
         self.reynolds = 220.
         self.influx_v = 0.4  # the velocity in lattice units of the ingoing flow
 
-        # self.lattice_velocity = [(x,y) for y in [-1,0,1] for x in [-1,0,1]]
-        #
-        # self.lattice_weight = [1/9 for i in range(9)]
-        # self.lattice_weight[::2] = [1/36 for i in self.lattice_weight[::2]]
-        # self.lattice_weight[4] = 4/9
-
         self.masked_off = np.zeros((self.shape[0], self.shape[1]), dtype=bool)
-        self.masked_off[[30:60,30:60]] = True
+        self.masked_off[10:15,30:40] = True
+        self.masked_off[10:15, 44:60] = True
+
+        self.init_lattice()
+
+    def init_lattice(self):
+        self.velocity = np.fromfunction(lambda x,y,d: np.sin(x/4)/10000 - np.cos(y/20)/10000, (self.shape[0], self.shape[1], 2))
+        self.flowin = flow_equilibrium(self.velocity)
 
     def update(self):
-        pass
+        self.flowin, self.velocity = update(self.flowin, self.masked_off, steps=1)
+        return self.velocity
--- Lattice Boltzmann/latticeboltzmann.py
+++ Lattice Boltzmann/latticeboltzmann.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numba import njit
+from numba import njit, jit
 
 lattice_velocity = np.array([(x, y) for y in [-1, 0, 1] for x in [-1, 0, 1]], dtype=np.float)
 
@@ -7,32 +7,123 @@ lattice_weight = np.array([1 / 9 for i in range(9)])
 lattice_weight[::2] = [1 / 36 for i in lattice_weight[::2]]
 lattice_weight[4] = 4 / 9
 
-VELOCITY_C = 1 / np.sqrt(3)
+VELOCITY_C = 1 / np.sqrt(3)  # sound velocity/ not sure why 2/sqrt(3) works but 1/sqrt(3) doesn't
 TIME_C = 1
 
 @njit()
-def collision(flow):
-    flow = flow.copy()
-    density = 0
-    velocity = np.array([0.,0.])
-    for i in range(9):
-        density += flow[i]
-        velocity += flow[i]*lattice_velocity[i]
-    velocity/=density
+def collision(flowin):
+    # print("colliding")
+    flowout = flowin.copy()
+    xsize, ysize, connections = flowin.shape
+    flow_eq = flowin.copy()*0
+    for x in range(xsize):
+        for y in range(ysize):
+            # if mask[x,y]: continue
+            density = 0
+            velocity = np.array([0., 0.])
+            for i in range(connections):
+                density += flowin[x, y, i]
+                velocity += flowin[x, y, i] * lattice_velocity[i]
+            if density != 0: velocity /= density
+            velocity/=2
+            # else: velocity*=0
+            for i in range(connections):
+                product = lattice_velocity[i] @ velocity
+                velocity_squared = velocity @ velocity
+                flow_eq[x,y,i] = lattice_weight[i] * density * (
+                        1+product / (VELOCITY_C ** 2) +
+                        product**2 / (2 * VELOCITY_C ** 4) -
+                        velocity_squared / (2 * VELOCITY_C ** 2)
+                )
+    flowout -= 1 / TIME_C * (flowin - flow_eq)
+    return flowout
 
-    flow_eq = []
-    for i in range(9):
-        product = lattice_velocity[i] @ velocity.T
-        velocity_squared = velocity @ velocity.T
-        flow_eq.append(
-            lattice_weight[i]*density*(
-                1 + product / VELOCITY_C ** 2 + \
-                np.square(product) / (2 * VELOCITY_C ** 4) -\
-                velocity_squared/(2 * VELOCITY_C**2)
+@njit()
+def flow_equilibrium(velocity, density=1):
+    xsize, ysize, dimensions = velocity.shape
+    connections = 9
+    flow_eq = np.empty((xsize, ysize, connections))
+
+    for x in range(xsize):
+        for y in range(ysize):
+            for i in range(connections):
+                product = lattice_velocity[i] @ velocity[x,y].T
+                velocity_squared = velocity[x,y] @ velocity[x,y].T
+                flow_eq[x, y ,i] = lattice_weight[i] * density * (
+                        1 + product / VELOCITY_C ** 2 +
+                        np.square(product) / (2 * VELOCITY_C ** 4) -
+                        velocity_squared / (2 * VELOCITY_C ** 2)
+                )
+    return flow_eq
+
+@njit()
+def obstacle_flip(flowin, flowout, mask):
+    xsize, ysize, connections = flowin.shape
+    for x in range(xsize):
+        for y in range(ysize):
+            if mask[x,y]:
+                for i in range(connections):
+                    flowout[x,y,8-i] = flowin[x,y,i]
+    return flowout
+
+@njit()
+def streaming(flowout):
+    # print('streaming')
+    xsize, ysize, connections = flowout.shape
+    flowin = np.empty_like(flowout)
+    for x in range(xsize):
+        for y in range(ysize):
+            for i in range(connections):  # periodic boundarie
+                x_neighbour = (x+int(lattice_velocity[i][0]))%xsize
+                y_neighbour = (y+int(lattice_velocity[i][1]))%ysize
+                flowin[int(x_neighbour), int(y_neighbour), i] = flowout[x, y, i]
+                flowin[x_neighbour, y_neighbour, 4] = flowout[x_neighbour, y_neighbour, 4]
+    return flowin
+
+@jit()
+def boundary_correction(flowin):
+    # left wall equilibriate
+    xsize, ysize, connections = flowin.shape
+
+    density = np.sum(flowin[0,:,:], axis=-1)
+    left_wall_velocity = np.empty((ysize, 2))
+    left_wall_velocity[:,:] = np.sum(flowin[0,:,:,None] * lattice_velocity[None, :], axis=1)
+    left_wall_velocity/=density[:,None]
+
+    flow_eq = np.zeros((ysize, 9))
+
+    for y in range(ysize):
+        for i in range(connections):
+            product = lattice_velocity[i] @ left_wall_velocity[y].T
+            velocity_squared = left_wall_velocity[y] @ left_wall_velocity[y].T
+            a = lattice_weight[i] * density[i] * (
+                    1 + product / VELOCITY_C ** 2 +
+                    product**2 / (2 * VELOCITY_C ** 4) -
+                    velocity_squared / (2 * VELOCITY_C ** 2)
             )
-        )
-        flow[i] -= 1/TIME_C * (flow[i] - flow_eq[i])
-    return flow
+            # print(a, product, velocity_squared, lattice_weight[i])
+            flow_eq[y, i] = a
 
-def streaming(content, neighbours):
-    pass
+    flowin[0,:,0:3] = 0.001#flow_eq[:,0:3]
+
+    #  right wall
+    # flowin[-1, :, 6:9] = flowin[-2, :, 6:9]
+    flowin[-2,:,:] = 0
+
+    return flowin
+
+# @njit()
+def update(flowin, mask, steps=10):
+    for step in range(steps):
+        flowin = boundary_correction(flowin)  # not working for left wall
+        #inflow # u[:, 0, :] = vel[:, 0, :]  # Left wall: compute density from known populations.
+                # rho[0, :] = 1. / (1. - u[0, 0, :]) * (sumpop(fin[i2, 0, :]) + 2. * sumpop(fin[i1, 0, :]))
+
+        flowout = collision(flowin)
+
+        flowout = obstacle_flip(flowin, flowout, mask)
+
+        flowin = streaming(flowout)
+    density = np.sum(flowin, axis=-1)
+    velocity = np.sum(flowin[:,:,:,None] * lattice_velocity[None,None,:], axis=2)/density[:,:,None]
+    return flowin, velocity
