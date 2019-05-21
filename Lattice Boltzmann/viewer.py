5668d6adeb68af5c73b9ec43957514f6d6650963
Rickrdem <36420046+Rickrdem@users.noreply.github.com>
2019-05-21 11:25:07 +0200

    Include methods to save mp4 by ffmpeg

--- Lattice Boltzmann/viewer.py
+++ Lattice Boltzmann/viewer.py
@@ -1,3 +1,4 @@
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -5,9 +6,6 @@ from matplotlib import animation
 
 def density(flow):
     mag = np.sum(flow, axis=-1)
-    # mag -= np.min(mag)
-    # mag /= np.max(mag)
-    # mag = np.nan_to_num(mag)
     return mag
 
 def normalised_magnitude(velocity):
@@ -15,16 +13,40 @@ def normalised_magnitude(velocity):
     return mag
 
 class Viewer():
+    """
+    Visualizer.
+
+    This class contains the methods required to visualize the Lattice Boltzman method.
+
+    :param simulation_state (): Contains all parameters of the simulation.
+    """
+    MAXFRAMES = 1800  # 30 sec@60fps
+
     def __init__(self, state):
         self.state = state
+        self.save_video = False
         self._init_matplotlib()
 
     def _init_matplotlib(self):
-        self.fig = plt.figure()
-        self.ax = plt.imshow(normalised_magnitude(self.state.velocity).T, cmap=cm.Reds, animated=True, vmin=0, vmax=0.08)
-        self.fig.colorbar(self.ax)
+        self.fig = plt.figure(figsize=(10,3))
+        ax = plt.gca()
+        self.ax = ax.imshow(normalised_magnitude(self.state.velocity).T, cmap=cm.Reds, animated=True, vmin=0., vmax=0.08)
+        self.fig.colorbar(self.ax, ax=ax)
 
-        self.animation_handler = animation.FuncAnimation(self.fig, self.update, blit=True, interval=1)
+        self.animation_handler = animation.FuncAnimation(self.fig, self.update, blit=True, interval=1, save_count=1800)
+#        plt.show()
+
+        if self.save_video:  # Requires ffmpeg package
+            now = datetime.datetime.now()
+            writer_init = animation.writers['ffmpeg']
+            writer = writer_init(fps=60, metadata=dict(artist='Me'), bitrate=1800)
+            self.animation_handler.save('{y}{m}{d}_{h}{min}{s}.mp4'.format(y=now.year,
+                                                                           m=now.month,
+                                                                           d=now.day,
+                                                                           h=now.hour,
+                                                                           min=now.minute,
+                                                                           s=now.second), writer=writer)
+
 
     def update(self, *args):
         self.state.update()
